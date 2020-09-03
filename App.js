40399da6a02397f696f657c232636ccbbefe268c
Aayush789 <58034477+Aayush789@users.noreply.github.com>
2020-09-03 19:09:18 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SignupLoginScreen from './screens/SignupLoginScreen'
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {AppTabNavigator} from './Components/AppTabNavigator'
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const switchNavigator = createSwitchNavigator({
+  SignupLoginScreen: {screen:SignupLoginScreen},
+ //BottomTab: {screen: AppTabNavigator}
+})
+
+const AppContainer= createAppContainer(switchNavigator)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
